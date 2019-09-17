1e0792441f83e9b3eba9032c1b5012b573bc14f1
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-09-17 13:37:18 -0700

    Initial Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-
+import { Route } from "react-router-dom";
 import SavedList from './Movies/SavedList';
+import MovieList from "./Movies/MovieList";
+import MovieCard from "./Movies/MovieCard";
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +14,10 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      {/* Routes added here. exact so that Movie List does not render on all movie pages*/}
+      <Route exact path="/" component={MovieList}/>
+        {/* */}
+        <Route path="/movies/ :id" render={props =><MovieCard {...props}movie={savedList} />}/>
     </div>
   );
 };
